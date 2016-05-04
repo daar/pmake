90a8a53641890219248148b08d77bb352dadc976
daar <dhkblaszyk@zeelandnet.nl>
2016-05-04 09:46:59 +0200

    Improved parsing of FPC output. Now all message numbers are assigned to a message level and couloured appropriately. The output can be controlled by ORing the message levels. Executing FPC is now done via TProcess Renamed ExecuteFPC to ExecuteTarget Added init_make and free_make procedures for internal structure setup and breakdown

--- fmake.pp
+++ fmake.pp
@@ -3,25 +3,25 @@ program fmake;
 {$mode objfpc}{$H+}
 { $define debug}
 
-uses
-  {$ifdef debug}
-  HeapTrc,
-  {$endif}
-  SysUtils, ufmake;
+uses {$IFDEF UNIX}
+  cthreads, {$ENDIF} {$ifdef debug}
+  HeapTrc, {$endif}
+  Classes,
+  SysUtils,
+  ufmake,
+  Process;
 
 var
-  fname: string;
+  fname: string = '';
   f: Text;
-  fpc: string;
-  sw : string = '';
-  BasePath: string;
+  BasePath: string = '';
 
   procedure FileSearch(const path: string);
   var
     info: TSearchRec;
     fi: Text;
     c: char;
-    tmp: String;
+    tmp: string;
   begin
     if FindFirst(path + '*', faAnyFile, info) = 0 then
     begin
@@ -33,10 +33,14 @@ var
             if info.name = 'FMake.txt' then
             begin
               tmp := path + info.name;
-              writeln('-- Found ', StringReplace(tmp, BasePath, '.' + DirectorySeparator, [rfReplaceAll]));
+              writeln('-- Found ', StringReplace(tmp, BasePath, '.' +
+                DirectorySeparator, [rfReplaceAll]));
               assign(fi, path + info.name);
               reset(fi);
-              writeln(f, '  add_subdirectory(''' + ExtractFilePath(path + info.name) + ''');');
+              writeln(f, '  add_subdirectory(''' +
+                ExtractFilePath(path + info.name) + ''');');
+
+
               while not EOF(fi) do
               begin
                 read(fi, c);
@@ -49,12 +53,18 @@ var
           //start the recursive search
           if (info.name <> '.') and (info.name <> '..') then
             FileSearch(IncludeTrailingBackSlash(path + info.name));
+
         until FindNext(info) <> 0
       finally
         FindClose(info);
       end;
     end;
   end;
+
+var
+  fpc_out: TStrings;
+  param: TStrings;
+  fpc_msg: TFPList;
 
 begin
   fname := GetTempFileName('.', 'fmake');
@@ -64,7 +74,6 @@ begin
   writeln(f, 'program make;');
   writeln(f, 'uses ufmake;');
   writeln(f, 'begin');
-  writeln(f, '  init_make;');
 
   BasePath := IncludeTrailingBackSlash(GetCurrentDir);
   FileSearch(BasePath);
@@ -73,12 +82,28 @@ begin
   writeln(f, 'end.');
   Close(f);
 
+  param := TStringList.Create;
+  param.Add('-viq');
+
   //build the makefile
+
   {$ifdef debug}
-  sw := '-gh';
+  param.Add('-gh');
   {$endif}
-  fpc := ExeSearch('fpc', SysUtils.GetEnvironmentVariable('PATH'));
-  ExecuteProcess(fpc, fname + ' -omake ' + sw, []);
+
+  param.Add(fname);
+
+  //based on the app extension of fmake, add the same extension to make
+  param.Add('-omake' + ExtractFileExt(ParamStr(0)));
+
+  fpc_out := RunFPCCommand(param);
+  param.Free;
+
+  fpc_msg := ParseFPCCommand(fpc_out);
+  fpc_out.Free;
+
+  WriteFPCCommand(fpc_msg, [mFail]);
+  fpc_msg.Free;
 
   //remove the object and source files
   {$ifndef debug}
--- ufmake.pas
+++ ufmake.pas
@@ -7,22 +7,51 @@ interface
 
 uses
   {$IFDEF UNIX}
-  cthreads;
-
+  cthreads,
   {$ENDIF}
+  Classes, Crt;
+
+type
+
+  TMessage = (mCompiling, mDebug, mError, mFail, mHint, mInformation, mLinking,
+    mNote, mOption, mUnitInfo, mUnknown, mWarning);
+  TMessages = set of TMessage;
+
+  TFPCOutput = record
+    msgno: integer;
+    msgtype: TMessage;
+  end;
+
+  TFPCColor = record
+    msgtype: TMessage;
+    msgcol: integer;
+  end;
+
+  TFPCMessage = record
+    msgidx: integer;
+    Text: string;
+  end;
+  PFPCMessage = ^TFPCMessage;
+
+const
+  mAll = [mCompiling, mDebug, mError, mFail, mHint, mInformation, mLinking,
+    mNote, mOption, mUnitInfo, mUnknown, mWarning];
 
 procedure add_executable(Name: string; files: array of const);
 procedure add_library(Name: string; files: array of const);
 procedure add_subdirectory(Name: string);
-procedure init_make;
 procedure project(Name: string);
 procedure run_make;
 procedure target_link_libraries(Name: string; files: array of const);
 
+function RunFPCCommand(Parameters: TStrings): TStrings;
+function ParseFPCCommand(FPCOutput: TStrings): TFPList;
+procedure WriteFPCCommand(FPCMsgs: TFPList; ShowMsg: TMessages; progress: double = -1);
+
 implementation
 
 uses
-  Crt, SysUtils, Classes, fpmkunit, Process;
+  SysUtils, fpmkunit, Process;
 
 type
   TTargetType = (ttExe, ttUnit);
@@ -47,6 +76,24 @@ type
     progress: double;
   end;
 
+const
+  MsgCol: array [TMessage] of TFPCColor = (
+    (msgtype: mCompiling; msgcol: Green),
+    (msgtype: mDebug; msgcol: LightGray),
+    (msgtype: mError; msgcol: White),
+    (msgtype: mFail; msgcol: Yellow),
+    (msgtype: mHint; msgcol: LightGray),
+    (msgtype: mInformation; msgcol: LightGray),
+    (msgtype: mLinking; msgcol: Red),
+    (msgtype: mNote; msgcol: LightGray),
+    (msgtype: mOption; msgcol: LightGray),
+    (msgtype: mUnitInfo; msgcol: LightGray),
+    (msgtype: mUnknown; msgcol: LightGray),
+    (msgtype: mWarning; msgcol: LightGray));
+
+//to add more FPC versions, ifdef this include file
+{$i fpc300.inc}
+
 var
   fbuild: TBuild;
   active_target: TTarget;
@@ -68,7 +115,7 @@ begin
     );
 end;
 
-Function UnitsOutputDir(BasePath: string; ACPU: TCPU; AOS: TOS): String;
+function UnitsOutputDir(BasePath: string; ACPU: TCPU; AOS: TOS): string;
 begin
   Result := IncludeTrailingBackSlash(BasePath + 'units') + MakeTargetString(ACPU, AOS);
   if not ForceDirectories(Result) then
@@ -78,7 +125,7 @@ begin
   end;
 end;
 
-Function BinOutputDir(BasePath: string; ACPU: TCPU; AOS: TOS): String;
+function BinOutputDir(BasePath: string; ACPU: TCPU; AOS: TOS): string;
 begin
   Result := IncludeTrailingBackSlash(BasePath + 'bin') + MakeTargetString(ACPU, AOS);
   if not ForceDirectories(Result) then
@@ -88,9 +135,128 @@ begin
   end;
 end;
 
+procedure WriteFPCCommand(FPCMsgs: TFPList; ShowMsg: TMessages; progress: double = -1);
+
+  function GetFPCMsgType(msgidx: integer): TMessage;
+  begin
+    if msgidx = -1 then
+      Result := mUnknown
+    else
+      Result := Msg[msgidx].msgtype;
+  end;
+
+  function GetFPCMsgCol(msgidx: integer): TMessage;
+  begin
+    if msgidx = -1 then
+      Result := mUnknown
+    else
+      Result := Msg[msgidx].msgtype;
+  end;
+
+var
+  i: integer;
+  fpc_msg: TFPCMessage;
+  fpc_msgtype : TMessage;
+begin
+  for i := 0 to FPCMsgs.Count - 1 do
+  begin
+    fpc_msg := TFPCMessage(FPCMsgs[i]^);
+    fpc_msgtype := GetFPCMsgType(fpc_msg.msgidx);
+    if fpc_msgtype in ShowMsg then
+    begin
+      if (fpc_msgtype = mCompiling) and (progress >= 0) and (progress <= 100) then
+        Write(format('[%3.0f%%]', [progress]));
+
+      if fpc_msg.msgidx <> -1 then
+        TextColor(MsgCol[fpc_msgtype].msgcol);
+      writeln(fpc_msg.Text);
+      NormVideo;
+    end;
+  end;
+end;
+
+function ParseFPCCommand(FPCOutput: TStrings): TFPList;
+var
+  sLine, snum: string;
+  found: boolean;
+  i: integer;
+  FPCmsg: PFPCMessage;
+  msgidx: integer;
+  ipos: SizeInt;
+begin
+  Result := TFPList.Create;
+
+  for i := 0 to FPCOutput.Count - 1 do
+  begin
+    sLine := FPCOutput[i];
+
+    found := False;
+    for msgidx := Low(Msg) to High(Msg) do
+    begin
+      snum := Format('(%d)', [Msg[msgidx].msgno]);
+      ipos := Pos(snum, sLine);
+      if ipos <> 0 then
+      begin
+        sLine := Copy(sLine, ipos + Length(snum), Length(sLine) - ipos - Length(snum) + 1);
+        sLine := StringReplace(sLine, BasePath, '.' + DirectorySeparator, [rfReplaceAll]);
+        found := True;
+        break;
+      end;
+    end;
+
+    FPCmsg := GetMem(sizeof(TFPCMessage));
+
+    if found then
+      FPCmsg^.msgidx := msgidx
+    else
+      FPCmsg^.msgidx := -1;
+
+    FPCmsg^.Text := sLine;
+    Result.Add(FPCmsg);
+  end;
+end;
+
+function RunFPCCommand(Parameters: TStrings): TStrings;
+const
+  BUF_SIZE = 2048; // Buffer size for reading the output in chunks
+
+var
+  AProcess: TProcess;
+  OutputStream: TStream;
+  BytesRead: longint;
+  Buffer: array[1..BUF_SIZE] of byte;
+  i: integer;
+begin
+  for i := 1 to BUF_SIZE do
+    Buffer[i] := 0;
+
+  AProcess := TProcess.Create(nil);
+  AProcess.Executable := fpc;
+  AProcess.Parameters.AddStrings(Parameters);
+  AProcess.Options := [poUsePipes];
+  AProcess.Execute;
+
+  OutputStream := TMemoryStream.Create;
+
+  repeat
+    BytesRead := AProcess.Output.Read(Buffer, BUF_SIZE);
+    OutputStream.Write(Buffer, BytesRead)
+  until BytesRead = 0;
+
+  AProcess.Free;
+
+  Result := TStringList.Create;
+
+  OutputStream.Position := 0;
+  Result.LoadFromStream(OutputStream);
+
+  // Clean up
+  OutputStream.Free;
+end;
+
 function FindDepTarget(Dependency: string): TTarget;
 var
-  i: Integer;
+  i: integer;
   target: TTarget;
 begin
   for i := 0 to fbuild.Targets.Count - 1 do
@@ -104,22 +270,19 @@ begin
   exit(nil);
 end;
 
-Function CompilerCommand(ATarget: TTarget; Source: string; TType: TTargetType): String;
-Var
-  Args: TStringList;
-  i: Integer;
+function CompilerCommand(ATarget: TTarget; Source: string; TType: TTargetType): TStringList;
+var
+  i: integer;
   targ: TTarget;
 begin
-  Args := TStringList.Create;
-  Args.Duplicates := dupIgnore;
-
-  Result := '';
+  Result := TStringList.Create;
+  Result.Duplicates := dupIgnore;
 
   // Output file paths
-  If TType = ttExe then
-    Args.Add('-FE' + BinOutputDir(ATarget.ActivePath, BuildCPU, BuildOS));
+  if TType = ttExe then
+    Result.Add('-FE' + BinOutputDir(ATarget.ActivePath, BuildCPU, BuildOS));
 
-  Args.Add('-FU' + ATarget.UnitsOutput);
+  Result.Add('-FU' + ATarget.UnitsOutput);
 
   //search other unit files for the dependencies
   for i := 0 to ATarget.Dep.Count - 1 do
@@ -132,35 +295,28 @@ begin
       halt(1);
     end;
 
-    Args.Add('-Fu' + targ.UnitsOutput);
+    Result.Add('-Fu' + targ.UnitsOutput);
   end;
 
   // Output executable name
-  If TType = ttExe then
+  if TType = ttExe then
   begin
-    Args.Add(Source);
-    Args.Add('-o' + ATarget.Name);
+    Result.Add(Source);
+    Result.Add('-o' + ATarget.Name);
   end;
 
   // compile unit name
-  If TType = ttUnit then
-    Args.Add(Source);
+  if TType = ttUnit then
+    Result.Add(Source);
 
   // Force the compiler-output to be easy parseable
   //if not Verbose then
-  args.Add('-viq');
-
-  // Convert to string
-  Result := '';
-  for i := 0 to Args.Count - 1 do
-    Result := Result + ' ' + Args[i];
-
-  Args.Free;
+  Result.Add('-viq');
 end;
 
 procedure add_executable(Name: string; files: array of const);
 var
-  i: Integer;
+  i: integer;
 begin
   //add a new target with files associated to it
   active_target := TTarget.Create;
@@ -187,7 +343,7 @@ end;
 
 procedure add_library(Name: string; files: array of const);
 var
-  i: Integer;
+  i: integer;
 begin
   //add a new target with files associated to it
   active_target := TTarget.Create;
@@ -218,14 +374,22 @@ end;
 
 procedure init_make;
 begin
-  fpc := ExeSearch('fpc', SysUtils.GetEnvironmentVariable('PATH'));
+  //{$note fix this for windows, allow to add commandline parameter}
+  //fpc := 'C:\Work\develop\fpc264\bin\i386-win32\fpc.exe';
 
+  if fpc = '' then
+    fpc := ExeSearch('fpc', SysUtils.GetEnvironmentVariable('PATH'));
+
+  if fpc = '' then
+  begin
+    writeln('error: cannot find the fpc compiler on searchpath');
+    writeln('       either set the PATH variable or use the commandline parameter -fpcbin');
+    halt(1);
+  end;
   fbuild := TBuild.Create;
   fbuild.Targets := TFPList.Create;
   fbuild.sfilecount := 0;
   fbuild.progress := 0;
-
-  //create the unit output path if needed
 end;
 
 procedure project(Name: string);
@@ -233,172 +397,13 @@ begin
   fbuild.projname := Name;
 end;
 
-function ExecFPC(Verbose: boolean; const Path: string; const ComLine: string;
-  const Env: TStrings; ConsoleOutput: TMemoryStream): integer;
+procedure ExecuteTarget(target: TTarget);
 var
-  P: TProcess;
-  BytesRead: longint;
-
-  function ReadFromStream(const ReadFromStdErr: boolean): longint;
-
-  const
-    READ_BYTES = 2048;
-
-  type
-    TMessages = (mCompiling, mLinking, mFatal);
-
-  var
-    //ifdef the MsgNum so it contains the correct message numbers for each compiler version.
-    MsgNum: array [TMessages] of integer = (3104, 9015, 10022);
-
-    n, available: longint;
-    BuffPos: longint;
-    sLine: string;
-    ch: char;
-    msg: TMessages;
-    ipos: integer;
-    snum: string;
-  begin
-
-    // try reading it
-    if ReadFromStdErr then
-    begin
-      available := P.Stderr.NumBytesAvailable;
-      // make sure we have room
-      if (bytesRead + Available > ConsoleOutput.Size) then
-        ConsoleOutput.SetSize(BytesRead + Available);
-      n := P.Stderr.Read((ConsoleOutput.Memory + BytesRead)^, available);
-    end
-    else
-    begin
-      available := P.Output.NumBytesAvailable;
-      // make sure we have room
-      if (bytesRead + Available > ConsoleOutput.Size) then
-        ConsoleOutput.SetSize(BytesRead + Available);
-      n := P.Output.Read((ConsoleOutput.Memory + BytesRead)^, available);
-    end;
-    if n > 0 then
-    begin
-      Inc(BytesRead, n);
-
-      sLine := '';
-      BuffPos := ConsoleOutput.Position;
-
-      //read lines from the stream
-      repeat
-        ConsoleOutput.Read(ch, 1);
-
-        if ch in [#10, #13] then
-        begin
-          if Verbose then
-            writeln(sLine)
-          else
-          begin
-            for msg := Low(TMessages) to High(TMessages) do
-            begin
-              snum := Format('(%d)', [MsgNum[msg]]);
-              ipos := Pos(snum, sLine);
-              if ipos <> 0 then
-              begin
-                sLine := StringReplace(sLine, BasePath,
-                  IncludeTrailingBackSlash('.'), [rfReplaceAll]);
-
-                case msg of
-                  mCompiling:
-                  begin
-                    Write(format('[%3.0f%%]', [fbuild.progress]));
-                    TextColor(Green);
-                  end;
-                  mLinking: TextColor(Red);
-                  mFatal:
-                  begin
-                    TextColor(White);
-                    writeln(sLine);
-                    halt(1);
-                  end;
-                end;
-                writeln(Copy(sLine, ipos + Length(snum), Length(sLine) -
-                  ipos - Length(snum) + 1));
-
-                NormVideo;
-              end;
-            end;
-          end;
-          if (LineEnding = #13#10) and (ch = #13) and
-            (ConsoleOutput.Position < BytesRead) then
-          begin
-            ConsoleOutput.Read(ch, 1);
-            if ch = #10 then
-              sLine := ''
-            else
-              sLine := ch;
-          end
-          else
-            sLine := '';
-          BuffPos := ConsoleOutput.Position;
-        end
-        else
-          sLine := sLine + ch;
-
-      until ConsoleOutput.Position >= BytesRead;
-
-      ConsoleOutput.Position := BuffPos;
-    end;
-
-    Result := n;
-  end;
-
-begin
-  fbuild.progress += 100 / fbuild.sfilecount;
-
-  Result := -1;
-  BytesRead := 0;
-  P := TProcess.Create(nil);
-  try
-    P.CommandLine := Path + ' ' + ComLine;
-    if assigned(Env) then
-      P.Environment.Assign(Env);
-
-    P.Options := [poUsePipes];
-
-    P.Execute;
-    while P.Running do
-    begin
-      // Only call ReadFromStream if Data from corresponding stream
-      // is already available, otherwise, on  linux, the read call
-      // is blocking, and thus it is not possible to be sure to handle
-      // big data amounts bboth on output and stderr pipes. PM.
-      if P.Output.NumBytesAvailable > 0 then
-        ReadFromStream(False)
-      else if P.StdErr.NumBytesAvailable > 0 then
-        ReadFromStream(True)
-      else
-        // no data, wait 100 ms
-        Sleep(100);
-    end;
-
-    // read last part
-    repeat
-    until ReadFromStream(False) = 0;
-
-    // read stderr
-    // JvdS: Note that this way stderr is added to the end of the stream. But I
-    // see no way showing the stderr output at the place it was actually written
-    repeat
-    until ReadFromStream(True) = 0;
-    ConsoleOutput.SetSize(BytesRead);
-
-    Result := P.ExitStatus;
-  finally
-    P.Free;
-  end;
-end;
-
-procedure ExecuteFPC(target: TTarget);
-var
-  i: Integer;
-  s: String;
+  i: integer;
   cmd: TMemoryStream;
+  param: TStringList;
+  fpc_out: TStrings;
+  fpc_msg: TFPList;
 begin
   cmd := TMemoryStream.Create;
 
@@ -406,16 +411,33 @@ begin
   target.UnitsOutput := UnitsOutputDir(target.ActivePath, BuildCPU, BuildOS);
   for i := 0 to target.units.Count - 1 do
   begin
-    s := CompilerCommand(target, target.ActivePath + target.units[i], ttUnit);
-    writeln(s);
-    ExecFPC(False, fpc, s, nil, cmd);
+    fbuild.progress += 100 / fbuild.sfilecount;
+    param := CompilerCommand(target, target.ActivePath + target.units[i], ttUnit);
+    //writeln(s);
+    fpc_out := RunFPCCommand(param);
+    param.Free;
+
+    fpc_msg := ParseFPCCommand(fpc_out);
+    fpc_out.Free;
+
+    WriteFPCCommand(fpc_msg, [mCompiling, mLinking, mFail], fbuild.progress);
+    fpc_msg.Free;
   end;
 
   //build exe
   if target.TargetType = ttExe then
   begin
-    s := CompilerCommand(target, target.Executable, ttExe);
-    ExecFPC(False, fpc, s, nil, cmd);
+    fbuild.progress += 100 / fbuild.sfilecount;
+    param := CompilerCommand(target, target.Executable, ttExe);
+
+    fpc_out := RunFPCCommand(param);
+    param.Free;
+
+    fpc_msg := ParseFPCCommand(fpc_out);
+    fpc_out.Free;
+
+    WriteFPCCommand(fpc_msg, [mCompiling, mLinking, mFail], fbuild.progress);
+    fpc_msg.Free;
   end;
 
   writeln('Built target ', target.Name);
@@ -423,11 +445,29 @@ begin
   cmd.Free;
 end;
 
+procedure free_make;
+var
+  i: integer;
+  target: TTarget;
+begin
+  //free all objects
+  for i := 0 to fbuild.Targets.Count - 1 do
+  begin
+    target := TTarget(fbuild.Targets[i]);
+    target.todo_dep.Free;
+    target.dep.Free;
+    target.units.Free;
+    target.Free;
+  end;
+  fbuild.Targets.Free;
+  fbuild.Free;
+end;
+
 procedure run_make;
 var
   target: TTarget;
-  i, j: Integer;
-  depname: String;
+  i, j: integer;
+  depname: string;
   done: integer = 0;
 begin
   if fbuild.projname = '' then
@@ -436,59 +476,44 @@ begin
     halt(1);
   end;
 
-  try
-    while done < fbuild.Targets.Count do
-    begin
+  while done < fbuild.Targets.Count do
+  begin
 
-      //determine the order to build, based on dependency count
-      i := 0;
-      target := TTarget(fbuild.Targets[i]);
-      while (target.todo_dep.Count > 0) or (target.done = True) do
+    //determine the order to build, based on dependency count
+    i := 0;
+    target := TTarget(fbuild.Targets[i]);
+    while (target.todo_dep.Count > 0) or (target.done = True) do
+    begin
+      Inc(i);
+      if i > fbuild.Targets.Count - 1 then
       begin
-        Inc(i);
-        if i > fbuild.Targets.Count - 1 then
-        begin
-          writeln('error: dependencies between remaining packages cannot be resolved!');
-          //make a dump here for all unresolved packages
-          halt(1);
-        end;
-        target := TTarget(fbuild.Targets[i]);
+        writeln('error: dependencies between remaining packages cannot be resolved!');
+        //make a dump here for all unresolved packages
+        halt(1);
       end;
-
-      ExecuteFPC(target);
-
-      target.Done := True;
-
-      depname := '';
-      if target.TargetType = ttUnit then
-        depname := target.Name;
-
-      //keep track of the amount of targets processed
-      Inc(done);
-
-      //remove depencency as it is resolved
-      if depname <> '' then
-        for i := 0 to fbuild.Targets.Count - 1 do
-        begin
-          target := TTarget(fbuild.Targets[i]);
-          j := target.todo_dep.IndexOf(depname);
-          if j <> -1 then
-            target.todo_dep.Delete(j);
-        end;
-
-    end;
-  finally
-    //free all objects
-    for i := 0 to fbuild.Targets.Count - 1 do
-    begin
       target := TTarget(fbuild.Targets[i]);
-      target.todo_dep.Free;
-      target.dep.Free;
-      target.units.Free;
-      target.Free;
     end;
-    fbuild.Targets.Free;
-    fbuild.Free;
+
+    ExecuteTarget(target);
+
+    target.Done := True;
+
+    depname := '';
+    if target.TargetType = ttUnit then
+      depname := target.Name;
+
+    //keep track of the amount of targets processed
+    Inc(done);
+
+    //remove depencency as it is resolved
+    if depname <> '' then
+      for i := 0 to fbuild.Targets.Count - 1 do
+      begin
+        target := TTarget(fbuild.Targets[i]);
+        j := target.todo_dep.IndexOf(depname);
+        if j <> -1 then
+          target.todo_dep.Delete(j);
+      end;
   end;
 end;
 
@@ -504,4 +529,9 @@ begin
   end;
 end;
 
+initialization
+  init_make;
+
+finalization
+  free_make;
 end.
